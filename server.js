a70f88f33308ebda3f7f49b943e0b334e38e64a0
Dave Seidman <daveseidman@gmail.com>
2018-11-28 12:46:36 -0500

    fix server path and port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
+
 const app = express();
 
-app.use(express.static('dist'));
+app.use('/qrtoqr', express.static(`${__dirname}/dist`));
 
-app.listen(8000);
+app.listen(9000, () => { console.log('qr-ar server listening on port 9000'); });
