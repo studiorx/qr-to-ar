19b499174b6ed19e47b617f6a2ef44b35aef0a6f
Dave Seidman <daveseidman@gmail.com>
2019-01-10 16:17:04 -0500

    updates

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use('/qrtoqr', express.static(`${__dirname}/dist`));
+app.use('/qrtoar', express.static(`${__dirname}/dist`));
+app.use('/qr-to-ar', express.static(`${__dirname}/dist`));
 
 app.listen(9000, () => { console.log('qr-ar server listening on port 9000'); });
